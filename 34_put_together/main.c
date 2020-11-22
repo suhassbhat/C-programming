d1c987607c1d19b368511294f959d9e3fbb5ca30
Coursera Learner <nobody@nowhere.nul>
2020-11-22 15:51:23 +0000

    Did Assignment 3

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -4,14 +4,79 @@
 #include "kv.h"
 #include "counts.h"
 #include "outname.h"
-
+void rmnlf (char * line)
+{
+  if(line[strlen(line)-1]=='\n')
+    {
+      line[strlen(line)-1]= '\0';
+    }
+}
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
-  //WRITE ME
-  return NULL;
+  size_t sz=0;
+  ssize_t len=0;
+  char * line=NULL;
+  char * a;
+  counts_t * c;
+  c=createCounts();
+  FILE *f = fopen(filename,"r");
+  if (f==NULL)
+    {
+      fprintf(stderr,"File does not exist\n");
+      exit(EXIT_FAILURE);
+    }
+  while((len=getline(&line,&sz,f))>=0)
+    {
+      if (strlen(line)>0);
+      {
+      rmnlf(line);
+      a=lookupValue(kvPairs,line);
+      addCount(c,a);
+      }}
+  free(line);
+  if(fclose(f)!=0)
+    {
+      fprintf(stderr,"File did not close\n");
+      exit(EXIT_FAILURE);
+    }
+  return c;
 }
 
 int main(int argc, char ** argv) {
-  //WRITE ME (plus add appropriate error checking!)
+  if(argc<3)
+    {
+      fprintf(stderr,"Incorrect input format\n");//WRITE ME (plus add appropriate error checking!)
+      exit(EXIT_FAILURE);
+    }
+  int i;
+  kvarray_t * k;
+  counts_t * c;
+  char * fname;
+  k=readKVs(argv[1]);
+  for(i=2;i<argc;i++)
+    {
+      fname=NULL;
+      c=countFile(argv[i],k);
+      fname=(char *) malloc(sizeof(char)*(strlen(argv[i])+8));
+      strcpy(fname,argv[i]);
+      strcat(fname,".counts");
+      FILE *f = fopen(fname,"w");
+      if (f==NULL)
+	{
+	  fprintf(stderr,"File did not open\n");
+	  exit(EXIT_FAILURE);
+	}
+      printCounts(c,f);
+      freeCounts(c);
+      free(fname);
+      if(fclose(f)!=0)
+	{
+	  fprintf(stderr,"File did not close\n");
+	  exit(EXIT_FAILURE);
+	}
+    }
+  freeKVs(k);
+      
+  
  //read the key/value pairs from the file named by argv[1] (call the result kv)
 
  //count from 2 to argc (call the number you count i)
