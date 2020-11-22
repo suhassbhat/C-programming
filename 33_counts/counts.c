c2c848d6ec5d93f9b885032a24d30d896cdcc7b2
Coursera Learner <nobody@nowhere.nul>
2020-11-22 07:43:11 +0000

    Did Assignments 33

--- 33_counts/counts.c
+++ 33_counts/counts.c
@@ -3,15 +3,58 @@
 #include <string.h>
 #include "counts.h"
 counts_t * createCounts(void) {
-  //WRITE ME
+  counts_t * c=NULL;
+  c = (counts_t *) malloc(sizeof(counts_t));
+  c->n=1;
+  c->a=NULL;
+  c->a = (one_count_t *) malloc(sizeof(one_count_t));
+  c->a->nvalue=NULL;
+  c->a->nvalue= (char *) malloc(11*sizeof(char));
+  strcpy((c->a->nvalue),"<unknown> ");
+  c->a->count=0;
+  return c;
 }
 void addCount(counts_t * c, const char * name) {
-  //WRITE ME
+  int i;
+  if (name==NULL)
+    {
+      c->a[0].count++;
+      return;
+    }
+  
+      for (i=1;i<c->n;i++)
+    {
+      if(strcmp(c->a[i].nvalue,name)==0)
+	{
+	  c->a[i].count++;
+	  return;
+	}
+    }
+      c->n++;
+      c->a = (one_count_t *) realloc(c->a,(c->n)*sizeof(one_count_t));
+      c->a[(c->n)-1].nvalue = (char *) malloc((strlen(name)+1)*sizeof(char));
+      strcpy(c->a[(c->n)-1].nvalue,name);
+      c->a[(c->n)-1].count = 1;
+      return;      
 }
 void printCounts(counts_t * c, FILE * outFile) {
-  //WRITE ME
+  int i;
+  for(i=1;i<c->n;i++)
+    {
+      fprintf(outFile,"%s: %d\n",c->a[i].nvalue,c->a[i].count);
+    }
+  if(c->a[0].count>0)
+    {
+      fprintf(outFile,"%s: %d\n",c->a[0].nvalue,c->a[0].count);
+    }
 }
 
 void freeCounts(counts_t * c) {
-  //WRITE ME
+  int i;
+  for(i=0;i<c->n;i++)
+    {
+      free(c->a[i].nvalue);
+    }
+  free(c->a);
+  free(c);
 }
--- 33_counts/counts.h
+++ 33_counts/counts.h
@@ -1,13 +1,14 @@
 #ifndef __COUNTS_H__
 #define __COUNTS_H__
 struct _one_count_t {
-  //DEFINE ME
-
+  char * nvalue;  //DEFINE ME
+  int count;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
-  //DEFINE ME
+  one_count_t * a;
+  int n;//DEFINE ME
 };
 typedef struct _counts_t counts_t;
 
